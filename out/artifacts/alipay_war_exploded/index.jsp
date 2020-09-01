092e25780e959ef1b18a8cdab7be9c2fc47ef605
3208711 <3208711@qq.com>
2020-09-01 17:14:39 +0800

    first commit

---
+++ out/artifacts/alipay_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: David
+  Date: 2020-09-01
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
---
+++ src/com/alipay/config/AlipayConfig.java
@@ -0,0 +1,14 @@
+package com.alipay.config;
+
+public class AlipayConfig {
+	public static String APPID = "2016101200667300";
+	public static String RSA_PRIVATE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0kUTUFLAsnb/6bO40yCFf1hQ0UySohX2tu4yPm9NKcCTckxHBlo9P7WpaYt4K0nsN18PoRneQ/ecOc3gPDXKFC9t3IKwb+qmhA99/YhfTKVY7ijQIth+PCZLFOp7ficto20DOmt2u+3puPEdEkZvG/lbNF+NkJaJTEYCbdeYbxE3lzXvG21mU1jWebWdgaLJssPS8Cv6/LHj6GJCBsM3SjYYeBvoKtp0c2SXiHb4vFiuX9JyoMpwg0UnFlPHPjcNzlW8xU2UJIc0KJsl3LVmCJHzZUNCcWYglgoqVjU2ZGXC2MIwDqzd/1+/f52hnquCPLlZnjXLf2PPrtY/UGNOaQIDAQAB";
+	public static String notify_url = "http://39.103.155.55:8080/notify_url.jsp";
+	public static String return_url = "http://39.103.155.55:8080/return_url.jsp";
+	public static String URL = "https://openapi.alipay.com/gateway.do";
+	public static String CHARSET = "UTF-8";
+	public static String FORMAT = "json";
+	public static String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtKDW86GFQHtkBpiz4ONOKH8ynmrRyNh3WYYYfqnpLrlnhMndo29Yu2Y7mL27Xh40cB91H+0AMjtppv150T0fyGURlsIus3BXMONSZrgyDL6TsuEYvUc82QRVYYWwsQxSz4Ws+rNBy1g4kNl5GdGLXVo0hMlzUnNw0KpBtDYyRF02hQf0lCuBWStmDaRq+p1YYCTm7nvh8o8NEyvZsZtGpa2YTBGr2W/DYplckZTf5D9MA4ivMpINHbdL70Ahz5E8brW4YdAfYf8Ocj2/g3k/xR7+JpPKDsWcrogUPym8Vfuql5qALBKT/eJ0fcf7T00y6zkvTyyebImgEw9+OmBvRQIDAQAB";
+	public static String log_path = "/log";
+	public static String SIGNTYPE = "RSA2";
+}
